cc8245c8357cd414a909a23b729f0f0fc6e1ebb2
Lambda San <120607663+lambda-san7@users.noreply.github.com>
2023-05-08 16:56:54 -0700

    Create write-to-file.js

---
+++ write-to-file.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var form = document.querySelector('form');
+  var textInput = document.querySelector('#text-input');
+  var submitBtn = document.querySelector('#submit-btn');
+  
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    var text = textInput.value;
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'write-to-file.php');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        console.log('File written successfully');
+      } else {
+        console.error('Error writing file');
+      }
+    };
+    xhr.send('text=' + encodeURIComponent(text));
+  });
+});
